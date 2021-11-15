3a1c6e8086f11ebc8213f769a76e2bad5ac3f680
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-15 14:19:45 -0600

    added login, signup, current user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   resources :users
+
+  get '/get-current-user', to: 'users#get_current_user'
+  post '/signup', to: 'users#create'
+  post '/login', to: 'sessions#create'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
