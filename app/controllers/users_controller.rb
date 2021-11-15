a1ed0288eb3203d4f944a2ff2fb00b16c9f26d49
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-15 14:56:36 -0600

    changed cors

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,6 +42,14 @@ class UsersController < ApplicationController
     @user.destroy
   end
 
+  def get_current_user
+    if logged_in?
+      render json: {user: current_user}, status: :ok
+    else
+      render json: {errors: ["User not logged in."]}, status: :ok
+    end
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
