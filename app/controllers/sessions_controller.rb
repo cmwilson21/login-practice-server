6b3df86d95aa12f546bdd3f8e307d4b001cd1ea2
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-14 21:45:02 -0600

    added some jwt logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,11 @@
 class SessionsController < ApplicationController
+  def create
+    @user = User.find_by_email(params[:email])
+    if @user && @user.authenticate(params[:password])
+      @token = encode_token(user_id: @user.id)
+      render json: {user: UserSerializer.new(@user), jwt: @token}, status: :ok
+    else
+      render json: {errors: ["Email or password did not match"]}, status: 422
+    end
+  end
 end
