5eeb5b6f0fb01dd9a26e8c503a67269dc74df386
Courtney Wilson <77312589+cmwilson21@users.noreply.github.com>
2021-11-14 21:47:00 -0600

    more jwt logic

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,13 +14,17 @@ class UsersController < ApplicationController
   end
 
   # POST /users
+  # POST /users
+  # signup
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: @user, status: :created, location: @user
+      @token = encode_token(user_id: @user.id)
+      # binding.pry
+      render json: {user: UserSerializer.new(@user), jwt: @token}, status: :created
     else
-      render json: @user.errors, status: :unprocessable_entity
+      render json: {errors: @user.errors.full_messages}, status: :unprocessable_entity
+      # render json: {errors: ["Please fill in each field."]}
     end
   end
 
